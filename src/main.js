600fdfdf2e9b8920080659bf14e3e101da1a1c5c
jible <jamezman88th@gmail.com>
2024-11-03 21:14:28 -0700

    slight progress towards finished preview pages but i am too tired to finish it now- still need to finish function for reading json and refine css alot

--- src/main.js
+++ src/main.js
@@ -1,7 +1,32 @@
+function makePreview(parent,currentproject){
+    const project = document.createElement("div");
+    parent.classList.add("project-item")
+    parent.append(project);
+
+
+    // Project title
+    const title = document.createElement('h3');
+    title.textContent = currentproject.name;
+    project.appendChild(title);
+
+    // Project github
+    const github = document.createElement('h3');
+    github.textContent = currentproject.github;
+    project.appendChild(github);
+
+    //image
+    for (img of currentproject.imgs){
+        const image = document.createElement('img');
+        image.src = img;
+        project.appendChild(image);
+    }
+
+    
+
+}
+
 // Fetch the JSON data
-fetch('src/projects.json') // Adjust the path if needed
-    .then((response) => response.json())  // Parse the JSON from the response
-    .then((data) => {
+fetch('src/projects.json').then((response) => response.json()).then((data) => {
         const projects = data.projects; // Access the projects array
         console.log(projects); // Log the projects array to verify the data
 
@@ -9,26 +34,7 @@ fetch('src/projects.json') // Adjust the path if needed
 
         // Example: Access each project item
         for (let i = 0; i < projects.length; i++){
-            const project = document.createElement("div");
-            proList.append(project);
-
-
-            // Project title
-            const title = document.createElement('h3');
-            title.textContent = projects[i].name;
-            project.appendChild(title);
-
-            // Project media
-            projects[i].title;
+            makePreview(proList, projects[i]);
         }
     })
     .catch((error) => console.error('Error loading JSON:', error));
-
-
-/*
-Games!
-
-https://emiree-draws.itch.io/tail-of-two-tides
-
-
-*/
