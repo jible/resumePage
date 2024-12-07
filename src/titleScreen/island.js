2f2bb18b9c95b1c2210d41ba988b85b7e1d78f0f
jible <jamezman88th@gmail.com>
2024-12-07 15:54:23 -0800

    slightly refactored style setting from js

--- src/titleScreen/island.js
+++ src/titleScreen/island.js
@@ -5,10 +5,6 @@ class islandElementType {
     }
 }
 
-const tree = new islandElementType( 'tree', "images/titleScreen/tree.png")
-
-
-
 class IslandElement{
     constructor(type,top,left){
         this.type = type
@@ -17,35 +13,67 @@ class IslandElement{
         this.create()
     }
     create(){
-        const offsetRotation = makeOffsetRotation(this.left,this.top);
+        const offsetRotation = this.makeOffsetRotation(this.left,this.top);
         island.appendChild(offsetRotation);
         
-        const islandElement = makeOrbitingObject(this.type)
+        const islandElement = this.makeOrbitingObject(this.type)
         offsetRotation.appendChild(islandElement)
     }
+
+
+    makeOffsetRotation(left,top){
+        const offsetRotation = document.createElement('div');
+        offsetRotation.classList.add('offset-rotation'); // Use shared class
+        offsetRotation.style.top = `${top}%`;
+        offsetRotation.style.left = `${left}%`;
+        return offsetRotation;
+    }
+
+    makeOrbitingObject(type){
+        const orbitingObject = document.createElement('img');
+        orbitingObject.setAttribute('src', type.imgSrc);
+        orbitingObject.classList.add('island-element');
+        orbitingObject.classList.add(type.cssClass);
+        return(orbitingObject)
+    }
+
+}
+
+
+//World Configuration
+const tree = new islandElementType( 'tree', "images/titleScreen/tree.png")
+
+const islandElementsInfo = [
+    [tree,50,50],
+    [tree,25,50],
+]
+
+const islandTraits = {
+    names: [
+        'spin-time',
+        'x-stretch',
+        'y-stretch',
+    ],
+    values:[
+        '20s',
+        '2',
+        '.5'
+    ]
 }
 
 
 // SETTING UP SCENE
 
-const islandTraits = {
-    spinTime: 20,
-    islandXStretch: 2,
-    islandYStretch: .5,
-}
-
-
-
 const island = document.getElementById("island");
 const varHolder = document.getElementById("title-screen");
-const islandElementsInfo = [
-    [tree,50,50],
-    [tree,25,50],
-]
-const islandElements = [];
-setIslandTraits();
-setUpIslandElements()
 
+
+
+const islandElements = setUpIslandElements();
+
+let style = '';
+style += setIslandTraits();
+varHolder.setAttribute('style' , style);
 
 
 
@@ -53,34 +81,26 @@ setUpIslandElements()
 
 // FUNCTION DECLARATIONS
 function setIslandTraits(){
-    varHolder.setAttribute('style',   
-        `--x-stretch: ${islandTraits.islandXStretch}; 
-        --y-stretch: ${islandTraits.islandYStretch};
-        --spin-time: ${islandTraits.spinTime}s;`  )
+    let style = '';
+    for (let i = 0; i < islandTraits.names.length; i++){
+        style += makeStyle(islandTraits.names[i], islandTraits.values[i])
+    }
+    return style;
 }
 
-
+function makeStyle( name, value){
+    return (`--${name}:${value}; `)
+}
 
 function setUpIslandElements(){
+    const islandElement = [];
     for (let i of islandElementsInfo){
         console.log(i)
         const newElement = new IslandElement(i[0],i[1],i[2]);
-        islandElements.push(newElement);
+        islandElement.push(newElement);
     }
+    return islandElement
 }
 
-function makeOffsetRotation(left,top){
-    const offsetRotation = document.createElement('div');
-    offsetRotation.classList.add('offset-rotation'); // Use shared class
-    offsetRotation.style.top = `${top}%`;
-    offsetRotation.style.left = `${left}%`;
-    return offsetRotation;
-}
 
-function makeOrbitingObject(type){
-    const orbitingObject = document.createElement('img');
-    orbitingObject.setAttribute('src', type.imgSrc);
-    orbitingObject.classList.add('island-element');
-    orbitingObject.classList.add(type.cssClass);
-    return(orbitingObject)
-}
+
