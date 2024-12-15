f291c98fab721445de5ba040a4d4db5f9f80a6b5
jible <jamezman88th@gmail.com>
2024-12-15 00:07:01 -0800

    broken- swapping to pc

---
+++ src/titleScreen/islandConfig.js
@@ -0,0 +1,76 @@
+import { buildings, landmarks, trees, houses } from "./islandTypes.js";
+
+
+export const islandElementsInfo = [
+    // Landmarks
+    [landmarks["salesForce"], 33, 55], 
+    [landmarks["coitTower"], 80, 50], 
+    [landmarks['transAmerica'], 55, 55], 
+    [landmarks['palcArts'], 70, 30],
+    [landmarks["lightHouse"], 8, 13], 
+    [landmarks['sutroTower'], 25, 35],
+    [landmarks['ggb'], 58, 90],
+    [landmarks['bofa'], 50, 60],
+
+    // Buildings
+    [buildings[4], 35, 70], 
+
+    // Houses
+    [houses[0], 50, 38], 
+    [houses[1], 55, 38],  
+    [houses[2], 55, 42],
+    [houses[3], 60, 42], 
+
+    // Trees
+    [trees[0], 45, 48], 
+    [trees[2], 43, 48], 
+    [trees[2], 42, 48], 
+    [trees[0], 41, 44], 
+    [trees[2], 44, 40], 
+    [trees[2], 85, 78], 
+    [trees[0], 77, 70], 
+    [trees[2], 79, 76], 
+    [trees[0], 77, 70], 
+    [trees[2], 68, 75], 
+    [trees[0], 70, 70], 
+    [trees[2], 75, 75], 
+    [trees[0], 82, 72],
+    [trees[1], 40, 28], 
+    [trees[1], 43, 28], 
+    [trees[1], 42, 25], 
+    [trees[1], 80, 80], 
+    [trees[1], 65, 80],
+    [trees[0], 64, 52],
+    [trees[0], 25, 51],
+    [trees[0], 43, 83],
+    [trees[2], 66, 83],
+    [trees[0], 80, 34],
+    [trees[1], 25, 20],
+    [trees[0], 11, 43],
+    [trees[2], 12, 35],
+    [trees[1], 84, 64],
+
+    [trees[2], 74, 24],
+    [trees[0], 63, 60],
+    [trees[1], 78, 45],
+    [trees[2], 21, 74],
+    [trees[0], 34, 21],
+    [trees[1], 72, 39],
+    [trees[0], 87, 43],
+    [trees[2], 28, 62],
+    [trees[1], 61, 34],
+    [trees[0], 14, 78],
+
+
+
+
+    // Buoys
+    [landmarks["buoy"], 90, 90], 
+    [landmarks["buoy"], 55, 75]
+];
+
+
+export const islandTraits = {
+    names: ['spin-time', 'spin-direction', 'x-stretch', 'y-stretch', 'x-unstretch', 'y-unstretch'],
+    values: ['500s', 'forwards','2.5', '0.5', '0.4', '2'],
+};
---
+++ src/titleScreen/islandTypes.js
@@ -0,0 +1,53 @@
+class IslandElementType {
+    constructor(imgSrc, height, width, offsetX, offsetY, secondSrc = null) {
+        this.imgSrc = imgSrc;
+        this.height = height;
+        this.width = width;
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        this.secondSrc = secondSrc;
+    }
+}
+
+// Centralized data configuration
+const ELEMENT_CONFIG = {
+    landmarks: {
+        tree: ["images/titleScreen/trees/tree2.png", "4vw", "4vw", "-50%", "-90%"],
+        salesForce: ["images/titleScreen/buildings/salesForce.png", "25vw", "25vw", "-50%", "-95%"],
+        coitTower: ["images/titleScreen/buildings/coitTower.png", "12vw", "12vw", "-50%", "-95%"],
+        transAmerica: ["images/titleScreen/buildings/transAmerica.png", "20vw", "20vw", "-50%", "-92%"],
+        palaceArts: ["images/titleScreen/buildings/palceArts.png", "6vw", "6vw", "-50%", "-90%"],
+        buoy: ["images/titleScreen/water/buoy.png", "2vw", "2vw", "-50%", "-90%"],
+        lightHouse: ["images/titleScreen/buildings/lightHouse.png", "7vw", "7vw", "-50%", "-90%"],
+        sutroTower: ["images/titleScreen/buildings/sutroTower.png", "15vw", "15vw", "-50%", "-100%"],
+        bofa: ["images/titleScreen/buildings/bofa.png", "10vw", "10vw", "-50%", "-100%"],
+        ggb: ["images/titleScreen/buildings/ggbSide.png", "20vw", "20vw", "-50%", "-100%", "images/titleScreen/buildings/ggbFront.png"]
+    },
+    buildings: [
+        "images/titleScreen/buildings/building1.png",
+        "images/titleScreen/buildings/building2.png",
+        "images/titleScreen/buildings/building3.png",
+        "images/titleScreen/buildings/building4.png",
+        "images/titleScreen/buildings/building5.png"
+    ],
+    houses: [
+        "images/titleScreen/buildings/houses/house1.png",
+        "images/titleScreen/buildings/houses/house2.png",
+        "images/titleScreen/buildings/houses/house3.png",
+        "images/titleScreen/buildings/houses/house4.png"
+    ],
+    trees: [
+        ["images/titleScreen/trees/tree1.png", "3vw", "3vw"],
+        ["images/titleScreen/trees/tree2.png", "6vw", "6vw"],
+        ["images/titleScreen/trees/tree3.png", "3vw", "3vw"]
+    ]
+};
+
+// Create IslandElementType instances
+export const landmarks = Object.fromEntries(
+    Object.entries(ELEMENT_CONFIG.landmarks).map(([key, value]) => [key, new IslandElementType(...value)])
+);
+
+export const buildings = ELEMENT_CONFIG.buildings.map(src => new IslandElementType(src, "5vw", "5vw", "-50%", "-100%"));
+export const houses = ELEMENT_CONFIG.houses.map(src => new IslandElementType(src, "3vw", "3vw", "-50%", "-100%"));
+export const trees = ELEMENT_CONFIG.trees.map(([src, height, width]) => new IslandElementType(src, height, width, "-50%", "-100%"));
